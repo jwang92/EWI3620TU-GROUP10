e6f21ac9700674a2d905bd81e080810c7b83dc74
HassanAlmahmoedi <H.Almahmoedi-1@student.tuelft.nl>
2013-11-14 12:17:46 +0100

    paar dingen

--- MazeRunner/src/GameStateManager.java
+++ MazeRunner/src/GameStateManager.java
@@ -1,37 +1,35 @@
 
 public class GameStateManager {
 	
-	public enum GameState {
-	    TITLE_STATE,
-	    MAINGAME_STATE,
-	    PAUSE_STATE
-	}
+	private int gamestate = 0;
 	
-	//public GameState m_curState = GameState.TITLE_STATE;
-	
-	void GameStateUpdate(GameState m_curState) {
+	void GameStateUpdate(GameState m_curSTATE) {
 	     // handle update
-	    switch(m_curState) {
+	    switch(m_curSTATE) {
 	       case TITLE_STATE:
 	          //
-	          if() {
-	              m_curState = GameState.MAINGAME_STATE;
-	          }
+	    	   //MainMenu.setTeller(0);
+	    	   gamestate = 0;
 	          break;
 	       case MAINGAME_STATE:
 	          //
-	          if() {
-	              m_curState = GameState.PAUSE_STATE;
-	          }
+	    	   //MainMenu.setTeller(1);
+	    	   gamestate = 1;
 	          break;
 	       case PAUSE_STATE:
 	           //
-	           if() {
-	               m_curState = GameState.MAINGAME_STATE;
-	           }
+	    	   //MainMenu.setTeller(2);
 	           break;
 	    }
 	}
 	
-	
+	public int getState(){
+		return gamestate;
+	}
+}
+
+enum GameState {
+    TITLE_STATE,
+    MAINGAME_STATE,
+    PAUSE_STATE
 }
