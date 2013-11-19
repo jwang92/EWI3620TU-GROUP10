f3fd1a69902e7964bfe755dd5322eb5aa846a15c
Ruben K <r.b.koeze@student.tudelft.nl>
2013-11-19 11:55:29 +0100

    Nieuwe Maze

--- MazeRunner/src/Maze.java
+++ MazeRunner/src/Maze.java
@@ -1,138 +1,133 @@
-import javax.media.opengl.GL;
-import com.sun.opengl.util.GLUT;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
-/**
- * Maze represents the maze used by MazeRunner.
- * <p>
- * The Maze is defined as an array of integers, where 0 equals nothing and 1 equals a wall.
- * Note that the array is square and that MAZE_SIZE contains the exact length of one side.
- * This is to perform various checks to ensure that there will be no ArrayOutOfBounds 
- * exceptions and to perform the calculations needed by not only the display(GL) function, 
- * but also by functions in the MazeRunner class itself.<br />
- * Therefore it is of the utmost importance that MAZE_SIZE is correct.
- * <p>
- * SQUARE_SIZE is used by both MazeRunner and Maze itself for calculations of the 
- * display(GL) method and other functions. The larger this value, the larger the world of
- * MazeRunner will be.
- * <p>
- * This class implements VisibleObject to force the developer to implement the display(GL)
- * method, so the Maze can be displayed.
- * 
- * @author Bruno Scheele, revised by Mattijs Driel
- *
- */
-public class Maze implements VisibleObject {
+import javax.media.opengl.GL;
+
+import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureCoords;
+import com.sun.opengl.util.texture.TextureData;
+import com.sun.opengl.util.texture.TextureIO;
+
+
+public class Maze  implements VisibleObject {
 	
-	public final double MAZE_SIZE = 10;
 	public final double SQUARE_SIZE = 5;
 
-	private int[][] maze = 
-	{	{  1,  1,  1,  1,  1,  1,  1,  1,  1,  1 },
-		{  1,  0,  0,  0,  0,  0,  0,  0,  0,  1 },
-		{  1,  0,  0,  0,  0,  0,  1,  1,  1,  1 },
-		{  1,  0,  1,  0,  0,  0,  1,  0,  0,  1 },
-		{  1,  0,  1,  0,  1,  0,  1,  0,  0,  1 },
-		{  1,  0,  1,  0,  1,  0,  1,  0,  0,  1 },
-		{  1,  0,  0,  0,  1,  0,  1,  0,  0,  1 },
-		{  1,  0,  0,  0,  1,  1,  1,  0,  0,  1 },
-		{  1,  0,  0,  0,  0,  0,  0,  0,  0,  1 },
-		{  1,  1,  1,  1,  1,  1,  1,  1,  1,  1 }	};
-
+	private WallList walls;
 	
-	/**
-	 * isWall(int x, int z) checks for a wall.
-	 * <p>
-	 * It returns whether maze[x][z] contains a 1.
-	 * 
-	 * @param x		the x-coordinate of the location to check
-	 * @param z		the z-coordinate of the location to check
-	 * @return		whether there is a wall at maze[x][z]
-	 */
-	public boolean isWall( int x, int z )
-	{
-		if( x >= 0 && x < MAZE_SIZE && z >= 0 && z < MAZE_SIZE )
-			return maze[x][z] == 1;
-		else
-			return false;
-	}
-	
-	/**
-	 * isWall(double x, double z) checks for a wall by converting the double values to integer coordinates.
-	 * <p>
-	 * This method first converts the x and z to values that correspond with the grid 
-	 * defined by maze[][]. Then it calls upon isWall(int, int) to check for a wall.
-	 * 
-	 * @param x		the x-coordinate of the location to check
-	 * @param z		the z-coordinate of the location to check
-	 * @return		whether there is a wall at maze[x][z]
-	 */
-	public boolean isWall( double x, double z )
-	{
-		int gX = convertToGridX( x );
-		int gZ = convertToGridZ( z );
-		return isWall( gX, gZ );
-	}
-	 
-	/**
-	 * Converts the double x-coordinate to its correspondent integer coordinate.
-	 * @param x		the double x-coordinate
-	 * @return		the integer x-coordinate
-	 */
-	private int convertToGridX( double x )
-	{
-		return (int)Math.floor( x / SQUARE_SIZE );
-	}
-
-	/**
-	 * Converts the double z-coordinate to its correspondent integer coordinate.
-	 * @param z		the double z-coordinate
-	 * @return		the integer z-coordinate
-	 */
-	private int convertToGridZ( double z )
-	{
-		return (int)Math.floor( z / SQUARE_SIZE );
-	}
-	
-	public void display(GL gl) {
-		GLUT glut = new GLUT();
-
-        // Setting the wall colour and material.
-        float wallColour[] = { 0.5f, 0.0f, 0.7f, 1.0f };				// The walls are purple.
-        gl.glMaterialfv( GL.GL_FRONT, GL.GL_DIFFUSE, wallColour, 0);	// Set the materials used by the wall.
-
-        // draw the grid with the current material
-		for( int i = 0; i < MAZE_SIZE; i++ )
-		{
-	        for( int j = 0; j < MAZE_SIZE; j++ )
-			{
-	            gl.glPushMatrix();
-				gl.glTranslated( i * SQUARE_SIZE + SQUARE_SIZE / 2, SQUARE_SIZE / 2, j * SQUARE_SIZE + SQUARE_SIZE / 2 );
-				if ( isWall(i, j) )
-					glut.glutSolidCube( (float) SQUARE_SIZE );
-				gl.glPopMatrix();
-			}
+	public void display(GL gl){
+		
+		walls = new WallList();
+		try {
+			walls.Read("testlevel2/Walls.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-		paintSingleFloorTile( gl, MAZE_SIZE * SQUARE_SIZE );			// Paint the floor.
+		
+		ArrayList<Wall> w = walls.getWalls();
+		
+		for(int i = 0; i < w.size(); i++){
+			
+			drawWall(gl, w.get(i).getStartx(), w.get(i).getStarty(), w.get(i).getEndx(), w.get(i).getEndy());
+			
+			
+		}
+		
+		drawFloor(gl);
+		
 	}
 	
-	/**
-	 * paintSingleFloorTile(GL, double) paints a single floor tile, to represent the floor of the entire maze.
-	 * 
-	 * @param gl	the GL context in which should be drawn
-	 * @param size	the size of the tile
-	 */
-	private void paintSingleFloorTile(GL gl, double size)
-	{
-        // Setting the floor color and material.
-        float wallColour[] = { 0.0f, 0.0f, 1.0f, 1.0f };				// The floor is blue.
-        gl.glMaterialfv( GL.GL_FRONT, GL.GL_DIFFUSE, wallColour, 0);	// Set the materials used by the floor.
-
+	public void drawWall(GL gl, int sx, int sy, int ex, int ey){
+		
+		float wallColour[] = { (float) Math.random(), (float) Math.random(), (float) Math.random() };				// The floor is blue.
+        gl.glMaterialfv( GL.GL_FRONT_AND_BACK, GL.GL_DIFFUSE, wallColour, 0);	// Set the materials used by the floor.
+        gl.glDisable(GL.GL_CULL_FACE);
         gl.glNormal3d(0, 1, 0);
 		gl.glBegin(GL.GL_QUADS);
-	        gl.glVertex3d(0, 0, 0);
-	        gl.glVertex3d(0, 0, size);
-	        gl.glVertex3d(size, 0, size);
-	        gl.glVertex3d(size, 0, 0);		
+	        gl.glVertex3d(sx * SQUARE_SIZE, 5, sy * SQUARE_SIZE);
+	        gl.glVertex3d(ex * SQUARE_SIZE, 5, ey * SQUARE_SIZE);
+	        gl.glVertex3d(ex * SQUARE_SIZE, 0, ey * SQUARE_SIZE);
+	        gl.glVertex3d(sx * SQUARE_SIZE, 0, sy * SQUARE_SIZE);		
 		gl.glEnd();	
-	}	
+		
+	}
+	
+	public void drawFloor(GL gl){
+		
+		File file = new File("textures/wood.png");
+		TextureData data = null;
+		try {
+			data = TextureIO.newTextureData(file, false, "png");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Texture woodTexture = TextureIO.newTexture(data);
+		woodTexture.setTexParameteri(GL.GL_TEXTURE_WRAP_S, GL.GL_REPEAT);
+		woodTexture.setTexParameteri(GL.GL_TEXTURE_WRAP_T, GL.GL_REPEAT);
+		woodTexture.setTexParameteri(GL.GL_TEXTURE_MAG_FILTER, GL.GL_NEAREST);
+		woodTexture.setTexParameteri(GL.GL_TEXTURE_MIN_FILTER, GL.GL_NEAREST);
+		TextureCoords coords = woodTexture.getImageTexCoords();
+		
+		
+		
+		float wallColour[] = { 1.0f, 1.0f, 1.0f};				
+        //gl.glMaterialfv( GL.GL_FRONT, GL.GL_DIFFUSE, wallColour, 0);	// Set the materials used by the floor.
+        
+        gl.glNormal3d(0, 1, 0);
+        gl.glEnable(GL.GL_TEXTURE_3D);
+        
+        woodTexture.bind();
+            
+		gl.glBegin(GL.GL_QUADS);
+        	gl.glTexCoord2f(coords.left(), coords.bottom());
+	        gl.glVertex3d(0, 0, 0);
+	        gl.glTexCoord2f(coords.left(), coords.top());
+	        gl.glVertex3d(0, 0, 50);
+	        gl.glTexCoord2f(coords.right(), coords.top());
+	        gl.glVertex3d(50, 0, 50);
+	        gl.glTexCoord2f(coords.right(), coords.bottom());
+	        gl.glVertex3d(50, 0, 0);
+		gl.glEnd();	
+		
+		woodTexture.disable();
+		
+	}
+	
+	
+	public boolean isWall( double x, double z )
+	{
+		
+		ArrayList<Wall> w = walls.getWalls();
+						
+		double distance;
+		
+		for(int i = 0; i < w.size(); i++){
+			
+			distance = distToSegment(x / SQUARE_SIZE, z / SQUARE_SIZE, w.get(i).getStartx(), w.get(i).getStarty(), w.get(i).getEndx(), w.get(i).getEndy());
+		
+			if(distance < 0.1){
+				System.out.println(distance);
+				return true;
+			}
+			
+		}
+		
+		return false;		
+		
+	}
+	
+	public double dist2(double vx, double vy, double wx, double wy) { return Math.pow(vx - wx, 2) + Math.pow(vy - wy, 2); }
+	public double distToSegmentSquared(double px, double py, double vx, double vy, double wx, double wy) {
+	  double l2 = dist2(vx, vy, wx, wy);
+	  if (l2 == 0) return dist2(px, py, vx, vy);
+	  double t = ((px - vx) * (wx - vx) + (py - vy) * (wy - vy)) / l2;
+	  if (t < 0) return dist2(px, py, vx, vy);
+	  if (t > 1) return dist2(px, py, wx, wy);
+	  return dist2(px, py, vx + t * (wx - vx), vy + t * (wy - vy));
+	}
+	public double distToSegment(double px, double py, double vx, double vy, double wx, double wy) { return Math.sqrt(distToSegmentSquared(px, py, vx, vy, wx, wy)); }
+	
+	
 }
